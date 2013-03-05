dc7169bd3959fec2aacb9deada820ae13fbdd67c
sct <sctsnipe@gmail.com>
2013-03-05 17:11:11 -0500

    Disabling cache controller

--- application/controllers/cache.php
+++ application/controllers/cache.php
@@ -2,6 +2,11 @@
 
 class Cache_Controller extends Base_Controller {
 
+	public function __construct() {
+		parent::__construct();
+		$this->filter('before','auth');
+	}
+
 	public function action_index()
 	{
 		//
