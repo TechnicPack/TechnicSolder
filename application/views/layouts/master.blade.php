e173009dddd6f4dbbc3f72b613296c8d6507ec34
AJ Fite <goldman60@nclf.net>
2014-05-08 16:33:48 -0700

    Make google stylesheet protocol agnostic
    removed the hard-coded http so it will play nice with https sites

--- application/views/layouts/master.blade.php
+++ application/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{ Asset::styles() }}
     {{ Asset::scripts() }}
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
   </head>
 
   <body>
