ad806749fbfd80068f9007a15d5da1dfe296bb70
GenPage <genpage@pagefortress.com>
2014-12-04 01:39:38 -0500

    Added Debug bar to config-sample

--- app/config-sample/app.php
+++ app/config-sample/app.php
@@ -121,6 +121,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Barryvdh\Debugbar\ServiceProvider',
 
 	),
 
@@ -161,6 +162,7 @@ return array(
 		'Cookie'            => 'Illuminate\Support\Facades\Cookie',
 		'Crypt'             => 'Illuminate\Support\Facades\Crypt',
 		'DB'                => 'Illuminate\Support\Facades\DB',
+		'Debugbar'          => 'Barryvdh\Debugbar\Facade',
 		'Eloquent'          => 'Illuminate\Database\Eloquent\Model',
 		'Event'             => 'Illuminate\Support\Facades\Event',
 		'File'              => 'Illuminate\Support\Facades\File',
