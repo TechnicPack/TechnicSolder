a20474fadadf7c00b5931a1cfd1b1ec4d92dba9f
Isigiel <l1uka4s@live.de>
2015-01-09 22:13:39 +0100

    Added Name Sorting on api level
    As suggested in Issue #228 this adds alphabetic sorting to mods in the API.

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -292,6 +292,11 @@ class APIController extends BaseController {
 			$build = Build::with('Modversions')
 						->where("modpack_id", "=", $modpack->id)
 						->where("version", "=", $build)->first();
+			$modverions = $build->modversions;
+			$modverions->each(function($modversion){
+				$modversion->name = $modversion->mod['name'];
+			});
+			$build->modversions = $modverions->sortBy('name');
 			if (empty($this->client))
 				Cache::put('modpack.'.$slug.'.build.'.$buildpass,$build,5);
 		}
