6e7843077977ad6f1536fcd56ecf3978c7816b50
GenPage <genpage@pagefortress.com>
2015-09-13 22:01:38 -0400

    Fix mod list table after latest version display change

--- app/views/mod/list.blade.php
+++ app/views/mod/list.blade.php
@@ -49,7 +49,6 @@
 						<br/>
 						<b>Latest Version:</b> {{ !empty($mod->versions) ? $mod->versions->first()->version : "N/A" }}
 					</td>
-					<td>{{ !empty($mod->versions) ? $mod->versions->first()->version : "N/A" }}</td>
 					<td>{{ !empty($mod->author) ? $mod->author : "N/A" }}</td>
 					<td>{{ !empty($mod->link) ? HTML::link($mod->link, $mod->link, array("target" => "_blank")) : "N/A" }}</td>
 					<td>{{ HTML::link('mod/view/'.$mod->id,'Manage', array("class" => "btn btn-xs btn-primary")) }}</td>
