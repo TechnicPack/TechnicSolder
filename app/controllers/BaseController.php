1d3bc34bdd25aeaf40c253f1dc129752a08a51d4
GenPage <genpage@pagefortress.com>
2015-12-06 00:07:21 -0500

    Version bump for v0.7.3.0

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -8,7 +8,7 @@ class BaseController extends Controller {
 			define('SOLDER_STREAM', 'DEV');
 		}
 		if(!defined('SOLDER_VERSION')) {
-			define('SOLDER_VERSION', 'v0.7.2.2');
+			define('SOLDER_VERSION', 'v0.7.3.0');
 		}
 
 		UpdateUtils::init();
