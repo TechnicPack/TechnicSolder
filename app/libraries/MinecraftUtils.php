5912ceeef10df655f4fb62056390b11dbe8365c0
Pedro Cunha <pedroagracio@gmail.com>
2018-10-13 13:05:24 +0100

    Fix Minecraft version updating

--- app/libraries/MinecraftUtils.php
+++ app/libraries/MinecraftUtils.php
@@ -19,31 +19,31 @@ class MinecraftUtils {
 	public static function getVersions() {
 		$response = array();
 
-		if (UrlUtils::checkRemoteFile('http://www.technicpack.net/api/minecraft', 15)['success']) {
-			$response = UrlUtils::get_url_contents('http://www.technicpack.net/api/minecraft', 15);
+		if (UrlUtils::checkRemoteFile('https://www.technicpack.net/api/minecraft', 15)['success']) {
+			$response = UrlUtils::get_url_contents('https://www.technicpack.net/api/minecraft', 15);
 			if($response['success']) {
 				$response = json_decode($response['data'], true);
-				krsort($response);
+				krsort($response, SORT_NATURAL);
 				Cache::put('minecraftversions', $response, 180);
 				return $response;
 			}
 		}
 
-		if (UrlUtils::checkRemoteFile('https://s3.amazonaws.com/Minecraft.Download/versions/versions.json', 15)['success']) {
-			$response = UrlUtils::get_url_contents('https://s3.amazonaws.com/Minecraft.Download/versions/versions.json', 15);
+		if (UrlUtils::checkRemoteFile('https://launchermeta.mojang.com/mc/game/version_manifest.json', 15)['success']) {
+			$response = UrlUtils::get_url_contents('https://launchermeta.mojang.com/mc/game/version_manifest.json', 15);
 			if($response['success']) {
 				$mojangResponse = json_decode($response['data'], true);
 				$versions = array();
 
-				foreach ($mojangResponse['versions'] as $versionEntry) {
-					if ($versionEntry['type'] != 'release') {
+				foreach ($mojangResponse['versions'] as $mojangVersion) {
+					if ($mojangVersion['type'] !== 'release') {
 						continue;
 					}
-					$mcVersion = $versionEntry['id'];
+					$mcVersion = $mojangVersion['id'];
 					$versions[$mcVersion] = array('version' => $mcVersion);
 				}
 
-				krsort($versions);
+				krsort($versions, SORT_NATURAL);
 				Cache::put('minecraftversions', $versions, 180);
 				return $versions;
 			}
