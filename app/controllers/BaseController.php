66e7891afc082bcb63d007b4f9c0e2942930fece
Pedro Cunha <pedroagracio@gmail.com>
2019-02-09 23:03:10 +0000

    Release v0.7.4.2

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -8,7 +8,7 @@ class BaseController extends Controller {
 			define('SOLDER_STREAM', 'DEV');
 		}
 		if(!defined('SOLDER_VERSION')) {
-			define('SOLDER_VERSION', 'v0.7.4.1');
+			define('SOLDER_VERSION', 'v0.7.4.2');
 		}
 
 		UpdateUtils::init();
