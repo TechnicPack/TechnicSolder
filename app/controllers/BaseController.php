2fa6f028e5a4b3fd3a062f428015c8d359ee717f
Pedro Cunha <pedroagracio@gmail.com>
2019-10-14 23:41:18 +0100

    Release v0.7.5.1

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -8,7 +8,7 @@ class BaseController extends Controller {
 			define('SOLDER_STREAM', 'DEV');
 		}
 		if(!defined('SOLDER_VERSION')) {
-			define('SOLDER_VERSION', 'v0.7.5');
+			define('SOLDER_VERSION', 'v0.7.5.1');
 		}
 
 		UpdateUtils::init();
