f69a683c944ff76f91dd9d48fedddec54bd83ef7
Pedro Cunha <pedroagracio@gmail.com>
2020-04-26 20:12:40 +0100

    that's a typo

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -15,7 +15,7 @@
 
   <body>
     <div class="container">
-      <h1>Look's like we've hit a snag!</h1>
+      <h1>Looks like we've hit a snag!</h1>
       <p>Something went terribly wrong when we were fetching the page you requested. Please try your request again in a few minutes or follow the help link to find your issue.</p>
       <p class="links"><a href="{{ URL::previous() }}" target="_blank"><i class="fa fa-arrow-circle-left"></i> Take me back to where I was!</a> <a href="https://github.com/TechnicPack/TechnicSolder/issues" target="_blank"><i class="fa fa-life-ring"></i> I need help!</a></p>
       <p class="meta"><span class="label"><i class="fa fa-fire"></i></span> 500 <span class="label"><i class="fa fa-cog"></i></span> {{ $exception->getMessage() }}</p>
