98bc5a74b32883cb3ca30026c7e467aa3903a207
Pedro Cunha <pedroagracio@gmail.com>
2020-04-26 20:08:34 +0100

    fix error pages

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -9,8 +9,8 @@
 
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     <link href="{{ URL::to('css/errors.css') }}" rel="stylesheet" type='text/css'>
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href='//fonts.googleapis.com/css?family=Raleway:400,300,100|Open+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,300,100|Open+Sans:400,700' rel='stylesheet' type='text/css'>
   </head>
 
   <body>
@@ -18,7 +18,7 @@
       <h1>Look's like we've hit a snag!</h1>
       <p>Something went terribly wrong when we were fetching the page you requested. Please try your request again in a few minutes or follow the help link to find your issue.</p>
       <p class="links"><a href="{{ URL::previous() }}" target="_blank"><i class="fa fa-arrow-circle-left"></i> Take me back to where I was!</a> <a href="https://github.com/TechnicPack/TechnicSolder/issues" target="_blank"><i class="fa fa-life-ring"></i> I need help!</a></p>
-      <p class="meta"><span class="label"><i class="fa fa-fire"></i></span> {{ $code }} <span class="label"><i class="fa fa-cog"></i></span> {{ $exception->getMessage() }}</p>
+      <p class="meta"><span class="label"><i class="fa fa-fire"></i></span> 500 <span class="label"><i class="fa fa-cog"></i></span> {{ $exception->getMessage() }}</p>
     </div>
   </body>
 </html>
