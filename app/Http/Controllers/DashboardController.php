f5f5b02236d91629177974d9877e8ebed2a81d66
Pedro Cunha <pedroagracio@gmail.com>
2020-04-27 16:52:46 +0100

    more query optimizations

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,14 +9,26 @@ class DashboardController extends Controller
 
     public function getIndex()
     {
-        $builds = Build::where('is_published', '=', '1')->orderBy('updated_at', 'desc')->take(5)->get();
+        $builds = Build::with('modpack')
+            ->withCount('modversions')
+            ->where('is_published', '=', '1')
+            ->orderBy('updated_at', 'desc')
+            ->take(5)
+            ->get();
 
-        $modversions = Modversion::whereNotNull('md5')->orderBy('updated_at', 'desc')->take(5)->get();
+        $modversions = Modversion::with('mod')
+            ->whereNotNull('md5')
+            ->orderBy('updated_at', 'desc')
+            ->take(5)
+            ->get();
 
         $rawChangeLog = UpdateUtils::getLatestChangeLog();
         $changelogJson = array_key_exists('error', $rawChangeLog) ? $rawChangeLog : array_slice($rawChangeLog, 0, 10);
 
-        return view('dashboard.index')->with('modversions', $modversions)->with('builds', $builds)->with('changelog', $changelogJson);
+        return view('dashboard.index')
+            ->with('modversions', $modversions)
+            ->with('builds', $builds)
+            ->with('changelog', $changelogJson);
     }
 
 }
