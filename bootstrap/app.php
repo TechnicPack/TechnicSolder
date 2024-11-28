7cf3e278d273b9d7aa126db739731fba35d4d14d
Shift <shift@laravelshift.com>
2024-11-28 17:45:11 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,32 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+
+        $middleware->throttleApi();
+        $middleware->api(\App\Http\Middleware\Cors::class);
+
+        $middleware->alias([
+            'build' => \App\Http\Middleware\Build::class,
+            'cors' => \App\Http\Middleware\Cors::class,
+            'modpack' => \App\Http\Middleware\Modpack::class,
+            'solder_clients' => \App\Http\Middleware\SolderClients::class,
+            'solder_keys' => \App\Http\Middleware\SolderKeys::class,
+            'solder_modpacks' => \App\Http\Middleware\SolderModpacks::class,
+            'solder_mods' => \App\Http\Middleware\SolderMods::class,
+            'solder_users' => \App\Http\Middleware\SolderUsers::class,
+        ]);
+
+        $middleware->priority([
+            StartSession::class,
+            ShareErrorsFromSession::class,
+            Authenticate::class,
+            ThrottleRequests::class,
+            AuthenticateSession::class,
+            SubstituteBindings::class,
+            Authorize::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
