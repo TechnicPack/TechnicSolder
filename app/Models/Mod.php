d62ba39d1947af7f6258383dcbbe3f70cedc0ec3
Shift <shift@laravelshift.com>
2024-11-28 17:45:17 +0000

    Adopt Laravel type hints

--- app/Models/Mod.php
+++ app/Models/Mod.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 
 class Mod extends Model
 {
     protected $guarded = [];
 
-    public function versions()
+    public function versions(): HasMany
     {
         return $this->hasMany(Modversion::class);
     }
