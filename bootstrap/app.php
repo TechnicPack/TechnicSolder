cb8f6a41590c4f890806798cce87d1f83db8bf56
Pedro Cunha <pedroagracio@gmail.com>
2024-11-28 21:18:01 +0000

    fix: remove ancient middleware priority
    That was from Laravel 6, was apparently removed in Laravel 7
    (https://github.com/laravel/laravel/blob/6.x/app/Http/Kernel.php)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,16 +30,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'solder_mods' => \App\Http\Middleware\SolderMods::class,
             'solder_users' => \App\Http\Middleware\SolderUsers::class,
         ]);
-
-        $middleware->priority([
-            StartSession::class,
-            ShareErrorsFromSession::class,
-            Authenticate::class,
-            ThrottleRequests::class,
-            AuthenticateSession::class,
-            SubstituteBindings::class,
-            Authorize::class,
-        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
