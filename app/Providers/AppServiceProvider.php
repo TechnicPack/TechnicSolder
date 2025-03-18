c4f3adbe8b04f094116a6fe792a0643c7f35d04a
Pedro Cunha <pedroagracio@gmail.com>
2025-03-18 19:07:45 +0000

    chore: release v0.8.8

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -38,7 +38,7 @@ class AppServiceProvider extends ServiceProvider
             define('SOLDER_STREAM', 'rolling');
         }
         if (! defined('SOLDER_VERSION')) {
-            define('SOLDER_VERSION', '0.8.7');
+            define('SOLDER_VERSION', '0.8.8');
         }
 
         View::composer('layouts.master', function ($view) {
